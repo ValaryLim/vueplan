95c4e9f33c792e0b29fd4fc54199822b636f52c2
Valary Lim <valarylim@outlook.com>
2020-02-22 17:23:39 +0800

    Remove changes from Reviews.vue

--- src/components/reviews/Reviews.vue
+++ src/components/reviews/Reviews.vue
@@ -1,18 +1,3 @@
 <template>
-    <div>
-        <ul>
-            <li v-for="element in allmodules" v-bind:key=element.code>
-                {{ element.code }}
-            </li>
-        </ul>
-    </div>
+    <h1>The Review Component</h1>
 </template>
-
-<script>
-export default {
-    name: "App",
-    display: "Reviews",
-    props: ['allmodules']
-}
-</script>
-
