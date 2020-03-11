c5f683db5be5d254ac3232a972e79db6214980b2
Valary Lim Wan Qian <valarylim@outlook.com>
2020-03-11 15:38:05 +0800

    Fix navbar styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,57 +1,128 @@
 <template>
-    <div id="header">
-        <div id="main">
-            <router-link to="/"><a id="main-first">vue</a><a id="main-second">Plan</a></router-link>
-        </div>
-        <b-navbar toggleable="lg" type="light" variant="light">
-            <!-- <router-link to="/" class="navbar-brand">vuePlan</router-link> -->
-
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-            <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-                <router-link to="/acadplan" activeClass="active" tag="li"><a class="nav-tab">Academic Plan</a></router-link>
-                <router-link to="/gradprogress" activeClass="active" tag="li"><a class="nav-tab">Graduation Progress</a></router-link>
-                <router-link to="/moduleinfo" activeClass="active" tag="li"><a class="nav-tab">Module Information</a></router-link>
-                <router-link to="/sepmapping" activeClass="active" tag="li"><a class="nav-tab">SEP Mapping</a></router-link>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav>
-                <b-nav-item-dropdown>
-                <!-- Using 'button-content' slot -->
-                <template v-slot:button-content>
-                    <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                </b-nav-item-dropdown>
-            </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
+    <div class="headerStyle">
+        <nav>
+            <div class="header-title">
+                <router-link to="/"><a class="header-title-first">vue</a><a class="header-title-second">Plan</a></router-link>
+            </div>
+            <div class="header-tabs">
+                <span class="header-tab"><router-link to="/acadplan"><a>Academic Plan</a></router-link></span>            
+                <span class="header-tab"><router-link to="/gradprogress"><a>Graduation Progress</a></router-link></span>
+                <span class="header-tab"><router-link to="/moduleinfo"><a>Module Information</a></router-link></span>
+                <span class="header-tab"><router-link to="/sepmapping"><a>SEP Mapping</a></router-link></span>
+                <span class="header-tab header-tab-dropdown">
+                    <button class="dropdown-button">User
+                        <font-awesome-icon icon="caret-down" />
+                    </button>
+                    <span class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </span>
+                </span>
+            </div>
+        </nav>
     </div>
 </template>
 
-<style>
-    #header {
-        background: #fff;
-    }
+<style scoped>
+/** main header style */
+.headerStyle {
+    overflow: hidden;
+    background: white;
+    padding: 10px;
+    text-align: center;
+    align-content: center;
+    margin-bottom: 30px;
+}
 
-    #main {
-        font-size: 60px;
-        font-weight: bold;
-        padding: 25px;
-    }
+/** header title - vuePlan */
+.header-title {
+    margin-top: 30px;
+    font-size: 60px;
+    font-weight: bold;
+}
 
-    #main #main-first {
-        color: grey;
-    }
+.header-title .header-title-first {
+    color: grey;
+}
 
-    #main #main-second {
-        color: #24305E;
-    }
+.header-title .header-title-second {
+    color: #24305E;
+} 
 
-    .nav-tab {
-        padding: 20px;
-    }
+/** header tabs */
+span {
+    padding: 20px;
+    display: inline-block;
+}
+
+a {
+    color: #24305E;
+}
+
+/* add bolding to tabs that are hovered */
+a:hover, .header-tab-dropdown:hover .dropdown-button {
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.router-link-active {
+    font-weight: bold;
+}
+
+/** header tabs - dropdown */
+.header-tab-dropdown {
+    float: inherit;
+}
+
+/* dropdown button */
+.header-tab-dropdown .dropdown-button {
+    border: none;
+    outline: none;
+    color: #24305E;
+    background-color: inherit;
+    font-family: inherit;
+}
+
+/* dropdown content (hidden by default) */
+.dropdown-content {
+  display: none;
+  float:inline-end;
+  position: absolute;
+  background-color: white;
+  min-width: 160px;
+  box-shadow: 0px 4px 8px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+/* links inside the dropdown */
+.dropdown-content a {
+  padding: 12px;
+  text-decoration: none;
+  display: block;
+  text-align: left;
+}
+
+/* add a grey background color to dropdown links on hover */
+.dropdown-content a:hover {
+    background-color: #EBEFF2;
+}
+
+/* show the dropdown menu on hover */
+.header-tab-dropdown:hover .dropdown-content {
+  display: block;
+}
 </style>
+
+<script>
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+library.add(faCaretDown);
+
+export default {
+    components: {
+        FontAwesomeIcon
+    },
+}
+</script>
