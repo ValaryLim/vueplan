677b987e79e52fd6387ffc7fdfe8ff30343c4b91
shinkurono <42473070+shinkurono@users.noreply.github.com>
2020-03-15 20:50:37 +0800

    ModuleInfo Review

--- src/components/moduleinfo/ModuleInfo.vue
+++ src/components/moduleinfo/ModuleInfo.vue
@@ -6,24 +6,17 @@ import modules from "../../assets/allmoduleinfo.json";
 import moduleReviews from '../../assets/moduleReviews.json';
 import users_table from '../../assets/users.json'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import * as Treeviz from 'treeviz';
-
 export default {
 	name: "App",
 	display: "Module Information",
 	components: {
-		FontAwesomeIcon,
-		Treeviz,
+		FontAwesomeIcon
 	},
 	data: function() {
 		return {
 			search:'',
 			mods: Object.values(modules),
-<<<<<<< HEAD
 			showModal: false,
-=======
-			show: true,
->>>>>>> 58e02f1f58f9f3f0b0c788b2399dc4833308b364
 			}
 		},
 
@@ -35,222 +28,75 @@ export default {
 			document.getElementById("res").innerHTML ="";
 		}
 		return Object.keys(this.allmodules).filter(mod => {
-			return this.allmodules[mod].fullname.toUpperCase().includes(this.search.toUpperCase())
+			return mod.toUpperCase().includes(this.search.toUpperCase())
             })
 		}
 	},
 	
 	methods:{
-		addOverlay:function(){
-			var over = document.getElementById('overlain')
-			over.insertAdjacentHTML('beforebegin', '<button id="show-modal" @click="showModal = true">Show Modal</button><modal v-if="showModal" @close="showModal = false"><h3 slot="header">custom header</h3></modal>')
-		},
-
 		modInfo:function(mod){
 			this.search ='';
-			this.show = false;
 			var res = document.getElementById("res");
-			window.scrollTo(0,0);
-
+			const overlay = document.querySelector('#overlay');
 			res.innerHTML = "<h1>" + mod.code + " " + mod.title + "</h1>";
-			res.insertAdjacentHTML('beforeend', '<div id = indicators>' + mod.department + " | " +  mod.mc + " MCs</div><hr></hr>");
-			res.insertAdjacentHTML('beforeend', "<p>" + mod.desc + "</p>");
-
+			res.insertAdjacentHTML('beforeend', '<div id = indicators>' + mod.department + " | " + "4 MCs</div><hr></hr>");
+			res.insertAdjacentHTML('beforeend', "<p>" + mod.desc + "</p>" + "<h2>Prerequisites</h2><hr></hr><p>");
 			//PREREQUISITES      
-			var arr = [];   
-			var arrInd = []   
-			if (mod.prereq != ""){
-				res.insertAdjacentHTML('beforeend', '<br>');
-				document.getElementById('res').insertAdjacentHTML('beforeend',"<h4>Prerequisites</h4><hr></hr>")
-
-				for(let i=0; i <this.mods.length; i++){
-					var index = mod.prereq.toUpperCase().indexOf(this.mods[i].code);
-					if(index != -1){
-						//check for those with modules that have same starting index
-						var check = arrInd.indexOf(index);
-						if(check != -1){
-							if (this.mods[i].code.length > arr[check][1].code.length){
-								arr.splice(check, 1);
-								arrInd.splice(check, 1);
-								arr.push([index,this.mods[i]]);
-								arrInd.push(index);
-							}
-						}else{
-							arr.push([index,this.mods[i]]);
-							arrInd.push(index);	
-						}
-					}
+			//getting the index the module is found at
+			var arr = [];      
+			for(let i=0; i <this.mods.length; i++){
+				var index = mod.prereq.toUpperCase().indexOf(this.mods[i].code);
+				if(index != -1){
+					arr.push([index,this.mods[i]])
 				}
-			
-				var startIndex = 0;
-				arr.sort(function(a,b) {return a[0]-b[0]});
-				if (arr.length == 0 && mod.prereq.length == 0) {
-					res.insertAdjacentHTML('beforeend', "None");
-				}
-
-
-				//joining the substrings to form the necessary output
-				for(let i = 0; i<arr.length; i++){ 
-					res.insertAdjacentHTML('beforeend',mod.prereq.substring(startIndex, arr[i][0]));        
-					var eventname = 'temp'.concat(i);
-					res.insertAdjacentHTML('beforeend',"<span id = '"+ eventname + "'>"+ arr[i][1].code+ "</span>");        
-					startIndex = arr[i][0] + arr[i][1].code.length;
-				}
-				document.getElementById("res").insertAdjacentHTML('beforeend', mod.prereq.substring(startIndex, mod.prereq.length) +"</h4>");
 			}
-			
-			
+		
+			var startIndex = 0;
+			arr.sort(function(a,b) {return a[0]-b[0]});
+			if (arr.length == 0 && mod.prereq.length == 0) {
+				res.insertAdjacentHTML('beforeend', "None");
+			}  
+			//joining the substrings to form the necessary output
+			for(let i = 0; i<arr.length; i++){ 
+				res.insertAdjacentHTML('beforeend',mod.prereq.substring(startIndex, arr[i][0]));        
+				var eventname = 'temp'.concat(i);
+				res.insertAdjacentHTML('beforeend',"<a id = '"+ eventname + "'href='#'>"+ arr[i][1].code+ "</a>");        
+				startIndex = arr[i][0] + arr[i][1].code.length;
+			}
 
-			//PRECLUSIONS	
+			document.getElementById("res").insertAdjacentHTML('beforeend', mod.prereq.substring(startIndex, mod.prereq.length) +"</h4>");
+      
+      
+      
+			//PRECLUSIONS
+			document.getElementById('res').insertAdjacentHTML('beforeend',"<h2>Preclusions</h2><hr></hr>")
 			var arr_a = [];
-			if (mod.preclusion != ""){
-				res.insertAdjacentHTML('beforeend', '<br><br>');
-
-				document.getElementById('res').insertAdjacentHTML('beforeend',"<h4>Preclusions</h4><hr></hr>")
-
-				for(let i=0; i <this.mods.length; i++){
-					index = mod.preclusion.toUpperCase().indexOf(this.mods[i].code);
-					if(index != -1){
-						if(this.mods[i].code != mod.code){
-							arr_a.push([index,this.mods[i]])
-						}
+			console.log(this.mods);
+			for(let i=0; i <this.mods.length; i++){
+				index = mod.preclusion.toUpperCase().indexOf(this.mods[i].code);
+				if(index != -1){
+					if(this.mods[i].code != mod.code){
+						arr_a.push([index,this.mods[i]])
 					}
 				}
-				startIndex = 0;
-				arr_a.sort(function(a,b) {return a[0]-b[0]});
-				if (arr_a.length == 0 && mod.preclusion.length == 0) {
-					res.insertAdjacentHTML('beforeend', "None");
-				}
-
-				//joining the substrings to form the necessary output
-				for(let i = 0; i<arr_a.length; i++){ 
-					res.insertAdjacentHTML('beforeend',mod.preclusion.substring(startIndex, arr_a[i][0]));        
-					eventname = 'temp'.concat(i);
-					res.insertAdjacentHTML('beforeend',"<span id = '"+ eventname + "'>"+arr_a[i][1].code+ "</span>");
-					startIndex = arr_a[i][0] + arr_a[i][1].code.length;
-				}
-				document.getElementById("res").insertAdjacentHTML('beforeend', mod.preclusion.substring(startIndex, mod.preclusion.length) +"</h4>");
-
 			}
-
-			var arr_all = arr.concat(arr_a).reverse();
-			//onclick of the module code, brings you to the module page      
-			var matches = document.querySelectorAll("span");
-			var x = this;
-			for (let j = arr_all.length-1; j >= 0; j--) {
-				let button = matches[matches.length-j-1];
-				button.addEventListener('click', function() {
-						x.modInfo(arr_all[j][1]);
-					}
-				)
+			startIndex = 0;
+			arr_a.sort(function(a,b) {return a[0]-b[0]});
+			if (arr_a.length == 0 && mod.preclusion.length == 0) {
+				res.insertAdjacentHTML('beforeend', "None");
 			}
-			//End Prerequisite/preclusion
-
-			res.insertAdjacentHTML('beforeend', '<br><br>');
-
-			//Workload
+			//joining the substrings to form the necessary output
+			for(let i = 0; i<arr_a.length; i++){ 
+				res.insertAdjacentHTML('beforeend',mod.preclusion.substring(startIndex, arr_a[i][0]));        
+				eventname = 'temp'.concat(i);
+				res.insertAdjacentHTML('beforeend',"<a id = '"+ eventname + "'href='#'>"+arr_a[i][1].code+ "</a>");
+				startIndex = arr_a[i][0] + arr_a[i][1].code.length;
+			}
 			res.insertAdjacentHTML('beforeend', '</p>')
+			
 			var workload = modules[mod.code]['workload'].split("-");
-
-			if (modules[mod.code]['workload']!=''){
-				var totalHours = workload.map(function(elt) {return parseInt(elt)}).reduce(function(a,b) {return a+b});
-				var totalString = totalHours + totalHours > 1 ? 'hrs' : 'hr';
-
-				res.insertAdjacentHTML('beforeend', '<h4>Weekly Workload - '+ totalHours  + totalString +'</h4>');
-
-				var col = ["#A3586D","#5C4A72","#F3B05A","#F4874B","#F46A4E"];
-				var names = ["Lec","Tut", "Lab","Proj","Prep"];
-
-				for (i = 0; i < 5; i++) {
-					for (var j = 0; j < workload[i]; j++){	
-						res.insertAdjacentHTML('beforeend', '<svg width="38" height="23"><rect width="37" height="23" fill="' + col[i] + '"/></svg>');
-					}
-				}
-
-
-				res.insertAdjacentHTML('beforeend', '<p id = labels></p>');
-				var label = document.getElementById("labels");
-
-				for (i = 0; i < 5; i++) {
-					if (workload[i] == 1){
-						label.insertAdjacentHTML('beforeend', '<span style="color:'+ col[i] + '">' + names[i] + '&nbsp;'.repeat(3) + '</span>');
-					}else if (workload[i] >1){			
-						label.insertAdjacentHTML('beforeend', '<span style="color:'+ col[i] + '">' + names[i] + '&nbsp;'.repeat((workload[i]-1)*8 + 3) + '</span>');
-					}
-				}
-			}
-			res.insertAdjacentHTML('beforeend', '<br>');
-
-			
-			
-			//TREE
-
-			var lst1 = Object.keys(this.allmodules).filter(module => {
-				return this.allmodules[module].prereq.toUpperCase().includes(mod.code)
-			});
-
-			var lst = []
-			for(i in lst1){
-				if (lst1[i] != mod.code){
-					lst.push(lst1[i]);
-				}
-			}
-
-			if(lst.length > 0){
-				var data_1 = [
-					{id: 1, text_1: mod.code, father: null, color:"#FF5722" },
-				]
-				var count = 2;
-				for(var i in lst){
-					data_1.push({id: count, text_1: this.allmodules[lst[i]].code, father: 1, color:"#FFC107"});
-					count++;
-				}
-
-				res.insertAdjacentHTML('beforeend','<h4>'+'Dependent Modules' +'</h4>');
-
-
-				var len = lst.length*40;
-				if (len != 0){
-					res.insertAdjacentHTML('beforeend', '<div id="tree" style="height:' + len + 'px; width:500px"></div> ');
-				}else{
-					res.insertAdjacentHTML('beforeend', '<div id="tree" style="height: 50px; width:500px"></div> ');
-				}
-
-				var myTree = Treeviz.create({
-					htmlId: "tree",
-					idKey: "id",
-					hasPan: false,
-					hasZoom:false,
-					hasFlatData: true,
-					relationnalField: "father",
-					nodeWidth:75,
-					nodeHeight:25,
-					mainAxisNodeSpacing:2,
-					isHorizontal:true,
-					renderNode: function(node) {
-					var result = "<div class='box' style='cursor:pointer;height:"
-						+node.settings.nodeHeight+ "px; width:"
-						+node.settings.nodeWidth
-						+"px;display:flex;flex-direction:column;justify-content:center;align-items:center;background-color:" 
-						+node.data.color
-						+ ";border-radius:px;'><div><strong>"
-					+node.data.text_1+
-					"</strong></div></div>";
-					return result;
-					},
-					linkShape:"curve",
-					onNodeClick : (nodeData) => x.modInfo(this.allmodules[nodeData.data.text_1])
-
-				});
-
-				myTree.refresh(data_1);
-			}
-
-
-			res.insertAdjacentHTML('beforeend', '<br>');
-
-			//Reviews section
-			console.log('Reviews');
+			var totalHours = workload.map(function(elt) {return parseInt(elt)}).reduce(function(a,b) {return a+b});
+			var totalString = totalHours + totalHours > 1 ? 'hrs' : 'hr';
 			var overallReviewNum = Object.values(moduleReviews[mod.code]).map(function(x) {return x['overall']}).reduce(function(a,b) {return a+b}) / Object.values(moduleReviews[mod.code]).length;
 			var avgStaffAdmin = Object.values(moduleReviews[mod.code]).map(function(x) {return x['staffadmin']}).reduce(function(a,b) {return a+b}) / Object.values(moduleReviews[mod.code]).length;
 			var avgContent = Object.values(moduleReviews[mod.code]).map(function(x) {return x['content']}).reduce(function(a,b) {return a+b}) / Object.values(moduleReviews[mod.code]).length;
@@ -258,13 +104,26 @@ export default {
 			overallReviewNum = Math.round(overallReviewNum*10)/10;
 			avgStaffAdmin = Math.round(avgStaffAdmin*10)/10;
 			avgContent = Math.round(avgContent*10)/10;
-
+			res.insertAdjacentHTML('beforeend', '<h2>Workload - '+ totalHours + ' ' + totalString +'</h2>');
+			res.insertAdjacentHTML('beforeend','<h3>'+workload+'</h3>');
+			res.insertAdjacentHTML('beforeend','<div>Workload viusalisation here</div><br></br>');
+			res.insertAdjacentHTML('beforeend','<h2>Prerequisite Tree</h2><hr></hr>');
+			res.insertAdjacentHTML('beforeend', '<div>Insert Prereq tree here</div><br></br>');
 			res.insertAdjacentHTML('beforeend', '<h2>Ratings and Reviews</h2><hr></hr>');
 			res.insertAdjacentHTML('beforeend','<h3 id = "OverallFeedbackNum">'+overallReviewNum+'</h3>');
 			res.insertAdjacentHTML('beforeend','<div id = "StarsOuter"><div id = "StarsInner"></div></div><div></div>');
 			res.insertAdjacentHTML('beforeend','<div id = "ContentFeedback">Learning Contents: ' + avgContent + '</div><div id = "sep"></div><div id = "StaffFeedback">Staff and Administration: ' + avgStaffAdmin + "</div><br></br>");
-			res.insertAdjacentHTML('beforeend', '<div id = "overlain"></div>')
-			res.insertAdjacentHTML('beforeend','<h4 id = "WrittenReviewsTitle">Written Reviews</h4><hr></hr><table><tbody id = "tabody">');
+			res.insertAdjacentHTML('beforeend', '<div id = "overlain"></div>');
+			res.insertAdjacentHTML('beforeend','<h4 id = "WrittenReviewsTitle">Written Reviews   <button id = userReview>Review this module now!</button></h4>');
+			const reviewMod = document.querySelector('#userReview');
+			reviewMod.addEventListener('click',function(){
+				overlay.style.display = 'block';
+				});
+			const closeReview = document.querySelector('#closeReview');
+			closeReview.addEventListener('click',function(){
+				overlay.style.display = 'none';
+				});
+			res.insertAdjacentHTML('beforeend','<hr></hr><table><tbody id = "tabody">');
 			var writtenReviews = {};
 			for (let [id, written] of Object.entries(moduleReviews[mod.code])) {
 				if (written['written'].length > 0) {
@@ -283,10 +142,20 @@ export default {
             const starPercentageRounded = `${(Math.round(starPercentage))}%`;
             document.querySelector("#StarsInner").style.width = starPercentageRounded;
 
-			//res.insertAdjacentHTML('beforeend','<div id = "StarsOuter"><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><i class="far fa-star"></i><div id = "StarsInner"><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i><i class="fas fa-star"></i></div></div><div></div>');
-			//res.insertAdjacentHTML('beforeend','<div id = "StarsOuter"><font-awesome-icon icon="times" /><font-awesome-icon icon="times" /><font-awesome-icon icon="times" /><font-awesome-icon icon="times" /><div id = "StarsInner"></div></div><div></div>');
-			//res.insertAdjacentHTML('beforeend',);
+			var arr_all = arr.concat(arr_a).reverse();
+			//onclick of the module code, brings you to the module page      
+			var matches = document.querySelectorAll("a");
+			var x = this;
+			for (let j = arr_all.length-1; j >= 0; j--) {
+				let button = matches[matches.length-j-1];
 
+				button.addEventListener('click', function() {
+						console.log(arr_all, "Array All")
+						console.log(arr_all[0][parseInt(j)], "Select Module");
+						x.modInfo(arr_all[j][1]);
+					}
+				)
+			}
 		}
 	}
 }
