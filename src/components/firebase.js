db28ebcdff74ab51d75d577eebdb34a538e3f2fc
Yan Jean <laiyanjean@gmail.com>
2020-03-15 14:17:28 +0800

    updated gitignore

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -3,6 +3,14 @@ import store from '../store.js';
 
 const firebaseConfig = {
   /* copy config details here */
+  apiKey: "AIzaSyBm9ZkRvTPHK5CQ-uZMSQt1RsekW9efOcs",
+  authDomain: "vueplan.firebaseapp.com",
+  databaseURL: "https://vueplan.firebaseio.com",
+  projectId: "vueplan",
+  storageBucket: "vueplan.appspot.com",
+  messagingSenderId: "475166877841",
+  appId: "1:475166877841:web:8556ab02c27653d0573730",
+  measurementId: "G-F3KW9HJ91K"
   };
   
   firebase.initializeApp(firebaseConfig);
